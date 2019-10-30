f6d2012e554e7096f6295ef82672c1b5ad87603e
Timothy Hendricks <tihendri@c2r3s1.wethinkcode.co.za>
2019-10-30 17:03:33 +0200

    trying to fix email verification

--- functions/create.php
+++ functions/create.php
@@ -48,22 +48,22 @@ $email = $_POST['email'];
     //   $sth->bindParam(':hash', $hash, PDO::PARAM_STR);
     //   $sth->execute();
 
+    $to = $_POST['email'];
+    $subject = 'Camagru | Registration';
+    $message = "
+    Your account has successfully been created. Your credential is as follows:
+    ------------------------
+    Username: '$_POST[login]'
+    ------------------------
+    Click on the following link to activate your account
+    http://localhost:8080/Camagru/verify.php?email=$_POST[mail]&hash=$hash
+    ";
+    $headers = 'From:tihendri@student.wethinkcode.co.za'."\r\n";
+    mail($to, $subject, $message, $headers);
   } catch (PDOException $e) {
       echo 'Error: '.$e->getMessage();
    //   exit;
   }
   
-  $to = $_POST['email'];
-  $subject = 'Camagru | Registration';
-  $message = "
-  Your account has successfully been created. Your credential is as follows:
-  ------------------------
-  Username: '$_POST[login]'
-  ------------------------
-  Click on the following link to activate your account
-  http://localhost:8080/Camagru/verify.php?email=$_POST[mail]&hash=$hash
-  ";
-  $headers = 'From:tihendri@student.wethinkcode.co.za'."\r\n";
-  mail($to, $subject, $message, $headers);
   //header("Location: ../index.php?err=Your account was created. Please activate it with the link sent to your email.\n");
 }
