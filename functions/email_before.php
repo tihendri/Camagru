389556e1e778552016a1462fd837e8d6a4782988
Timothy Hendricks <tihendri@student.wethinkcode.co.za>
2019-11-04 12:25:17 +0200

    saving this file for later, if I need it

---
+++ functions/email_before.php
@@ -0,0 +1,14 @@
+
+<?php
+  // if ($_GET[err]){echo "<script>alert(\"".htmlentities($_GET[err])."\");window.location.href = \"email_before.php\";</script>";}
+  // include_once "../header.php";
+?>
+<link rel="stylesheet" href="../styles/styles.css">
+<title>Camagru - Home</title>
+  <article class="main">
+    <p><b>Please check your email for a verification link, in order to activate your account.</b></p>
+  </article>
+  <aside class="aside">
+    
+  </aside>
+</div>
