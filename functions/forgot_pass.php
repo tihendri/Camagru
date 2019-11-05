deda61eb64335bddd1d09a662930f0525a276f69
Timothy Hendricks <tihendri@student.wethinkcode.co.za>
2019-11-05 11:55:57 +0200

    commented out code, will use later maybe

--- functions/forgot_pass.php
+++ functions/forgot_pass.php
@@ -1,38 +1,42 @@
 <?php
-  header('Location: ../index.php');
-  include_once '../config/database.php';
- 
- 
-function forgot_passwd() { 
+
+
+
+////////////////////////////////////////Not using this function///////////////////////////////////////////////////
+
+
+
+
+function forgot_passwd() {
+  // header('Location: ../index.php');
+  include_once '../config/database.php'; 
 //   if (empty($_POST[login])) {
 //       header("Location: ../forgot_pass.php?err=Please fill in all the spaces.\n");
 //      exit();
 //   }
+  // try {
+  //     $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+  //     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //     $sth = $dbh->prepare('SELECT COUNT(*) FROM users WHERE login = :login');
+  //     $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
+  //     $sth->execute();
+  // } catch (PDOException $e) {
+  //     echo 'Error: '.$e->getMessage();
+  //     exit;
+  // }
+  // if (!($result = $sth->fetchColumn())) {
+  //     header("Location: ../forgot_username.php?err=The account does not exist.\n");
+  //     exit();
+  // $hash = md5(rand(0, 1000));
   try {
-      $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sth = $dbh->prepare('SELECT COUNT(*) FROM users WHERE login = :login');
-      $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
-      $sth->execute();
-  } catch (PDOException $e) {
-      echo 'Error: '.$e->getMessage();
-      exit;
-  }
-  if (!($result = $sth->fetchColumn())) {
-      header("Location: ../forgot_username.php?err=The account does not exist.\n");
-      exit();
-  }
-  $hash = md5(rand(0, 1000));
-  try {
-      $sth = $dbh->prepare("UPDATE users SET forgot = '$hash' WHERE login = :login");
-      $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
-      $sth->execute();
-      $sth = $dbh->prepare('SELECT mail FROM users WHERE login = :login');
-      $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
-      $sth->execute();
-
-  $to = $_POST['email'];
-  $subject = 'Camagru | Registration';
+      // $sth = $dbh->prepare("UPDATE users SET forgot = '$hash' WHERE login = :login");
+      // $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
+      // $sth->execute();
+      // $sth = $dbh->prepare('SELECT mail FROM users WHERE login = :login');
+      // $sth->bindParam(':login', $_POST[login], PDO::PARAM_STR);
+      // $sth->execute();
+  
+    $email = $_POST['email'];
 //   $user = $sth->fetch();
 //   $to = $user[mail];
   $subject = 'Camagru | Forgot your password';
@@ -41,10 +45,10 @@ function forgot_passwd() {
   Username: '$_POST[username]'
   ------------------------
   Click on the following link to reactivate your account with a new password.
-  http://localhost:8080/Camagru/forgot_password2.php?email=$to&hash=$hash
+  http://localhost:8080/Camagru/forgot_password2.php?email=$email&hash=$hash
   ";
   $headers = 'From:tihendri@student.wethinkcode.co.za'."\r\n";
-  mail($to, $subject, $message, $headers);
+  mail($email, $subject, $message, $headers);
   echo "<script>window.alert('To change your password click on the link sent to your email.\n')</script>";
 //   header("Location: ../index.php?err=To change your password click on the link sent to your email.\n");
   } catch (PDOException $e) {
