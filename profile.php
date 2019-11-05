d20361c0b66cdeb5b3a1cd4d9604857b8abc4a2c
Timothy Hendricks <tihendri@student.wethinkcode.co.za>
2019-11-05 17:08:55 +0200

    profile page has picture upload done

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
   include_once "header.php";
   include_once "footer.php";
 ?>
-<title>Camagru - profile</title>
+<title>Camagru | Profile</title>
 <h2>My Profile</h2>
   <article class="main">
     <p></p>
@@ -11,4 +11,16 @@
   <aside class="aside">
     
   </aside>
-</div>
+<form action="" method="POST" enctype="multipart/form-data">
+  <input type="file" name="upl_img">
+  <button type="submit" name="upload">Upload</button>
+</form>
+<?php
+if (isset($_POST['upload'])) {
+  if (isset($_FILES['upl_img']['name'])) {
+    include "functions/upload_img.php";
+    echo"<script>window.alert('Your image has been successfully uploaded.')</script>";
+    upload_img($_SESSION['id']);
+  }
+}
+?>
