c1fec305bdbf5c8aaeb9d8e9a37fff7f1323cb57
Timothy Hendricks <tihendri@student.wethinkcode.co.za>
2019-11-06 16:46:23 +0200

    reordered profile page for required links

--- profile.php
+++ profile.php
@@ -5,22 +5,23 @@
 ?>
 <title>Camagru | Profile</title>
 <h2>My Profile</h2>
-  <article class="main">
+
+<ul class="profile_list">
+            <li><a href="upload_image.php">Upload Image</a></li>
+            <li><a href="#.php">Change Username</a></li>
+            <li><a href="#.php">Change Password</a></li>
+            <li><a href="#.php">Change Email</a></li>
+</ul>
+
+  <!-- <article class="main">
     <p></p>
   </article>
   <aside class="aside">
     
   </aside>
+
 <form action="" method="POST" enctype="multipart/form-data">
   <input type="file" name="upl_img">
   <button type="submit" name="upload">Upload</button>
 </form>
-<?php
-if (isset($_POST['upload'])) {
-  if (isset($_FILES['upl_img']['name'])) {
-    include "functions/upload_img.php";
-    echo"<script>window.alert('Your image has been successfully uploaded.')</script>";
-    upload_img($_SESSION['id']);
-  }
-}
-?>
+?> -->
