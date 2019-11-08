f26522f2feec50f06d5a979507a724740e391698
Timothy Hendricks <tihendri@c2r2s1.wethinkcode.co.za>
2019-11-08 13:33:04 +0200

    need to finish final layout

--- gallery.php
+++ gallery.php
@@ -1,19 +1,19 @@
 <?php
   session_start();
-  if ($_SESSION[Username] && !empty($_SESSION[Username])) {
-      if (!$_GET[page]) {
+  if ($_SESSION['username']) {
+      if (!$_GET['page']) {
           header('Location: Gallery.php?page=1');
       }
   } else {
       header('Location: index.php?err=You must login to access this page.');
   }
     include_once 'config/database.php';
-    $start = ($_GET[page] - 1) * 10;
+    $start_ = (($_GET['page'] - 1) * 10);
     try {
-        $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $dbh = new PDO("mysql:host=localhost;dbname=camagru", "root", "root1004");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sth = $dbh->prepare('SELECT * FROM snap LIMIT 10 OFFSET :start');
-        $sth->bindParam(':start', $start, PDO::PARAM_INT);
+        $sth = $dbh->prepare("SELECT * FROM pictures LIMIT 10 OFFSET 1");
+        $sth->bindParam(":start_", $start_, PDO::PARAM_INT);
         $sth->execute();
     } catch (PDOException $e) {
         echo 'Error: '.$e->getMessage();
@@ -21,8 +21,8 @@
     }
     $result = $sth->fetchAll();
     if (!$result) {
-        if ($_GET[page] > 1) {
-            $prev = $_GET[page] - 1;
+        if ($_GET['page'] > 1) {
+            $prev = $_GET['page'] - 1;
             header("Location: Gallery.php?page=$prev");
             exit();
         } else {
@@ -31,7 +31,7 @@
     }
     include_once 'header.php';
     ?>
-      <title>Camagru - Gallery</title>
+      <title>Camagru | Gallery</title>
       <article class='main'>
       <div class=container>
       <?php
@@ -39,19 +39,19 @@
           echo "<div class='fleximgbox'>";
           try {
               $sth = $dbh->prepare('SELECT COUNT(*) FROM likes WHERE img_id = :img_id');
-              $sth->bindParam(':img_id', $value[id], PDO::PARAM_INT);
+              $sth->bindParam(':img_id', $value['id'], PDO::PARAM_INT);
               $sth->execute();
           } catch (PDOException $e) {
               echo 'Error: '.$e->getMessage();
               exit;
           }
           $likes = $sth->fetchColumn();
-          if ($value[login] == $_SESSION[Username]) {
+          if ($value['username'] == $_SESSION['username']) {
               echo "<a href='user/remove_img.php?img=$value[id]&page=$_GET[page]'><img src='images/DeleteRed.png' width='42' style='position:absolute;'></a>";
           }
           echo "<img src='$value[img]' style='width:426px;'>
           <br/>
-          Poster par : <i>$value[login]
+          Picture by : <i>$value[username]
           <br/></i>Likes: $likes
           <a href='user/Like.php?img_id=$value[id]&page=$_GET[page]' style='float:right; margin-top: -20px;'><img src='images/Like.png' width='42' height='42'></a>
           <form class='comment' action='user/Comment.php?img_id=$value[id]&page=$_GET[page]' method='post'><br/>
@@ -69,7 +69,7 @@
           if ($result) {
               echo "<div class='comments'>";
               foreach ($result as $key => $value) {
-                  echo "-> <i>$value[login]</i> <br/> $value[comment] <hr>";
+                  echo "-> <i>$value[username]</i> <br/> $value[comment] <hr>";
               }
               echo '</div>';
           }
@@ -78,21 +78,21 @@
       echo '</div><center>
       <ul class="pagination">';
       try {
-          $sth = $dbh->prepare('SELECT COUNT(*) FROM snap');
+          $sth = $dbh->prepare('SELECT COUNT(*) FROM pictures');
           $sth->execute();
       } catch (PDOException $e) {
           echo 'Error: '.$e->getMessage();
           exit;
       }
       $nbpage = ($sth->fetchColumn() - 1) / 10 + 1;
-      $prev = $_GET[page] - 1;
+      $prev = $_GET['page'] - 1;
       if ($prev > 0) {
           echo "<li><a href='?page=$prev'>«</a></li>";
       }
       for ($i = 1; $i <= $nbpage; ++$i) {
           echo "<li><a href='?page=$i'>$i</a></li>";
       }
-      $next = $_GET[page] + 1;
+      $next = $_GET['page'] + 1;
       if ($next < $nbpage) {
           echo "<li><a href='?page=$next'>»</a></li>";
       }
