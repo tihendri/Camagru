281fc7dc3f7046cafeb3c9404d452d3ff5d10dbe
Timothy Hendricks <tihendri@c2r2s1.wethinkcode.co.za>
2019-11-11 10:36:16 +0200

    connection file for functions, does'nt want to work otherwise for some reason

---
+++ functions/connect_to_db.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=camagru", "root", "root1004");
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
