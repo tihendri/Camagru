b6bd733713a849423d35c0c75f75a8b947ae8ebd
Timothy Hendricks <tihendri@c2r2s1.wethinkcode.co.za>
2019-11-15 16:42:09 +0200

    next on the to-do list is updating user account

--- profile.php
+++ profile.php
@@ -7,12 +7,37 @@
 <h2>My Profile</h2>
 
 <ul class="profile_list">
-            <li><a href="upload_image.php">Upload Image</a></li>
-            <li><a href="edit_account.php">Edit Account</a></li>
-            <li>Username:  <?php echo $_SESSION['username'] ?></li>
-            <li>Email address:  <?php echo $_SESSION['email'] ?></li>
+            <li><a href="upload_image.php">Upload Image</a></li><br /><br />
+            <li><a href="edit_account.php">Edit Account</a></li><br /><br />
+            <li>Username:  <?php echo $_SESSION['username'] ?></li><br /><br />
+            <li>Email address:  <?php echo $_SESSION['email'] ?></li><br /><br />
 </ul>
+<section>
+  <div>
+  <?php
+    if (isset($_POST['updt_name']) || isset($_POST['updt_passwd']) || isset($_POST['updt_notif'])) {
+      include "functions/update_user";
+    }
+  ?>
 
+</div>
+
+
+
+<div class="main" style="text-align:center">
+<?php
+include "functions/img_display.php";
+      img_display();
+?>
+<?php
+    if (isset($_GET['session_status'])) {
+      if ($_GET['session_status'] == "logout") {
+        include "functions/disconnect.php";
+        disconnect();
+      }
+    }
+?>
+</div>
   <!-- <article class="main">
     <p></p>
   </article>
